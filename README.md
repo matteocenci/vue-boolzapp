f151f5dfcc51fdce71f48a784cae5f7fcdd910ea
matteocenci <matteocenci01@gmail.com>
2024-04-22 16:15:06 +0200

    feat: array contacts

---
+++ README.md
@@ -0,0 +1,9 @@
+### MILESTONE 1
+● Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e
+dall’interlocutore (bianco) assegnando due classi CSS diverse
+● Visualizzazione dinamica della lista contatti:
+
+### MILESTONE 2
+● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i
+messaggi relativi al contatto attivo all’interno del pannello della conversazione
+● Click sul contatto mostra la conversazione del contatto cliccato
---
+++ js/script.js
@@ -0,0 +1,169 @@
+console.log(Vue);
+
+const globalUsersList = [
+    {
+        name: 'Michele',
+        text: 'img/avatar_1.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Tutto fatto!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Fabio',
+        text: 'img/avatar_2.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Ciao come stai?',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+            },
+            {
+                date:'',
+                text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Samuele',
+        text: 'img/avatar_3.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'La Marianna va in campagna',
+                status: 'received'
+            },
+            {
+                date:'',
+                text: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Ah, scusa.',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Alessandro B.',
+        text: 'img/avatar_4.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Alessandro L.',
+        text: 'img/avatar_5.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Ricordati di chiamare la nonna',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Va bene, stasera la sento',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Claudia',
+        text: 'img/avatar_6.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Ciao Claudia, hai novità?',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Non ancora',
+                status: 'received'
+            },
+            {
+                date:'',
+                text: 'Nessuna nuova, buona nuova',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Federico',
+        text: 'img/avatar_7.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Fai gli auguri a Martina che è il suo compleanno!',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'Grazie per avermelo ricordato, e scrivo subito!',
+                status: 'received'
+            },
+        
+        ],
+    },
+    {
+        name: 'Davide',
+        text: 'img/avatar_8.jpg',
+        visible: true,
+        messages: [
+            {
+                date:'',
+                text:'Ciao, andiamo a mangiare la pizza stasera?',
+                status: 'received'
+            },
+            {
+                date:'',
+                text: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                status: 'sent'
+            },
+            {
+                date:'',
+                text: 'OK!!',
+                status: 'received'
+            }
+        
+        ],
+    }
+]
+
+
